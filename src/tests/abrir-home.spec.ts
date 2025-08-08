1e01e2599efd70848a7be6c7b918e0479a9c1eed
MS4M\jaime.rios <jaime.rios@ms4m.com>
2025-08-08 16:21:51 -0500

    add safe

--- src/tests/abrir-home.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '../hooks/global-hooks';
-import { OpenHomePage } from '../screenplay/tasks/OpenHomePage';
-
-test('Abrir página de Éxito y validar logo', async ({ page }) => {
-  await OpenHomePage.perform(page);
-});
